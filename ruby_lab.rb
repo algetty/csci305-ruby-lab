cbfd4485cf4aa36c33f86fdf72a1bcc62597e979
Takaia <takaia42@gmail.com>
2018-02-09 16:17:15 -0700

    setup/info/RuboCop

--- ruby_lab.rb
+++ ruby_lab.rb
@@ -4,55 +4,55 @@
 #
 # CSCI 305 - Ruby Programming Lab
 #
-# <firstname> <lastname>
-# <email-address>
+# Amelia Getty
+# gettyamelia@gmail.com
 #
 ###############################################################
 
 $bigrams = Hash.new # The Bigram data structure
-$name = "<firstname> <lastname>"
+$name = 'Amelia Getty'
 
 # function to process each line of a file and extract the song titles
 def process_file(file_name)
-	puts "Processing File.... "
+  puts 'Processing File.... '
 
-	begin
-		if RUBY_PLATFORM.downcase.include? 'mswin'
-			file = File.open(file_name)
-			unless file.eof?
-				file.each_line do |line|
-					# do something for each line (if using windows)
-				end
-			end
-			file.close
-		else
-			IO.foreach(file_name, encoding: "utf-8") do |line|
-				# do something for each line (if using macos or linux)
-			end
-		end
+  begin
+    if RUBY_PLATFORM.downcase.include? 'mswin'
+      file = File.open(file_name)
+      unless file.eof?
+        file.each_line do |line|
+          # do something for each line (if using windows)
+        end
+      end
+      file.close
+    else
+      IO.foreach(file_name, encoding: 'utf-8') do |line|
+        # do something for each line (if using macos or linux)
+      end
+    end
 
-		puts "Finished. Bigram model built.\n"
-	rescue
-		STDERR.puts "Could not open file"
-		exit 4
-	end
+    puts "Finished. Bigram model built.\n"
+  rescue
+    STDERR.puts 'Could not open file'
+    exit 4
+  end
 end
 
 # Executes the program
 def main_loop()
-	puts "CSCI 305 Ruby Lab submitted by #{$name}"
+  puts "CSCI 305 Ruby Lab submitted by #{$name}"
 
-	if ARGV.length < 1
-		puts "You must specify the file name as the argument."
-		exit 4
-	end
+  if ARGV.empty?
+    puts 'You must specify the file name as the argument.'
+    exit 4
+  end
 
-	# process the file
-	process_file(ARGV[0])
+  # process the file
+  process_file(ARGV[0])
 
-	# Get user input
+  # Get user input
 end
 
-if __FILE__==$0
-	main_loop()
+if __FILE__== $0
+  main_loop
 end
